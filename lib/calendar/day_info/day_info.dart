6ba8490ded0e9b70189759ccd648a58b96aa5cbc
Liam <wem@wemtechnology.com>
2020-07-15 16:47:24 -0400

    Add lesson scheduling button (no functionality yet)

--- lib/calendar/day_info/day_info.dart
+++ lib/calendar/day_info/day_info.dart
@@ -25,9 +25,8 @@ class DayInfoState extends State<DayInfo> {
             dayInfoBuilder(
                 context, state.calendarEventsState, widget.day));
   }
-}
 
-Widget dayInfoBuilder(context, CalendarEventsState calendarEventsState, DateTime day) {
+  Widget dayInfoBuilder(context, CalendarEventsState calendarEventsState, DateTime day) {
   List<Widget> infoContents = [];
   calendarEventsState.selectEventsForDay(day).forEach((event) {
     infoContents.add(EventCard(event: event));
@@ -39,14 +38,19 @@ Widget dayInfoBuilder(context, CalendarEventsState calendarEventsState, DateTime
           width: 400,
           child: Padding(
               padding: EdgeInsets.only(top: 10, left: 10, bottom: 20),
-              child: Text(
-                new DateFormat.MMMM().format(day) + " " + day.day.toString(),
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w800),
-              ))));
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    new DateFormat.MMMM().format(day) + " " + day.day.toString(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  scheduleLessonButton,
+              ]))));
 
   infoContents.insert(0, infoHeader);
 
@@ -58,3 +62,16 @@ Widget dayInfoBuilder(context, CalendarEventsState calendarEventsState, DateTime
           padding: EdgeInsets.all(3), child: ListView(children: infoContents)));
 }
 
+    final Widget scheduleLessonButton = Padding(
+        padding: EdgeInsets.all(10),
+        child: FlatButton(
+            child: Text("Schedule a lesson!", style: TextStyle(color: Colors.white)),
+            onPressed: () {},
+            color: Colors.red[600],
+            disabledColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                )));
+
+
+}
